7ac89a4304bd33f618edf527bd1c0bede9e0f37c
Hans Chan <chenhc@ucweb.com>
2016-04-01 15:31:48 +0800

    update example

--- examples/srilanka/bundle.js
+++ examples/srilanka/bundle.js
@@ -84,15 +84,24 @@
 	lk.point({
 	  _id: 'Colombo',
 	  coordinate: [79.8612430, 6.9270790]
-	}).point({
-	  _id: 'Galle',
-	  coordinate: [80.2209770, 6.0535190]
 	}).point([{
-	  _id: 'Ella',
-	  coordinate: [81.0465530, 6.8666990]
+	  _id: 'Anuradhapura',
+	  coordinate: [80.3881330, 8.3451850]
+	}, {
+	  _id: 'Trincomalee',
+	  coordinate: [81.1967960, 8.5922000]
 	}, {
 	  _id: 'Kandy',
 	  coordinate: [80.6337260, 7.2905720]
+	}, {
+	  _id: 'Galle',
+	  coordinate: [80.2209770, 6.0535190]
+	}, {
+	  _id: 'Ella',
+	  coordinate: [81.0465530, 6.8666990]
+	}, {
+	  _id: 'Tissamahaharama',
+	  coordinate: [81.2876690, 6.2791540]
 	}]).type([{
 	  _id: 'train',
 	  name: 'Train',
@@ -106,16 +115,78 @@
 	  // symbolSize: 3
 	  lineStyle: {
 	    normal: {
-	      color: '#000',
-	      width: 1,
+	      color: '#d14',
+	      width: 1.2,
 	      curveness: 0.2
+	    }
+	  }
+	}, {
+	  _id: 'bus',
+	  name: 'Bus',
+	  zlevel: 1,
+	  effect: {
+	    show: true,
+	    period: 6,
+	    trailLength: 0
+	  },
+	  // color: '#fff',
+	  // symbolSize: 3
+	  lineStyle: {
+	    normal: {
+	      color: '#004a5e',
+	      width: 1,
+	      curveness: 0.1
 	    }
 	  }
 	}]).line({
 	  type: 'train',
 	  from: 'Colombo',
 	  to: 'Galle'
-	});
+	}).line([{
+	  type: 'train',
+	  from: 'Colombo',
+	  to: 'Anuradhapura'
+	}, {
+	  type: 'train',
+	  from: 'Anuradhapura',
+	  to: 'Trincomalee'
+	}, {
+	  type: 'train',
+	  from: 'Colombo',
+	  to: 'Kandy'
+	}, {
+	  type: 'train',
+	  from: 'Kandy',
+	  to: 'Ella'
+	}, {
+	  type: 'train',
+	  from: 'Kandy',
+	  to: 'Anuradhapura'
+	}, {
+	  type: 'bus',
+	  from: 'Colombo',
+	  to: 'Kandy'
+	}, {
+	  type: 'bus',
+	  from: 'Kandy',
+	  to: 'Ella'
+	}, {
+	  type: 'bus',
+	  from: 'Colombo',
+	  to: 'Galle'
+	}, {
+	  type: 'bus',
+	  from: 'Ella',
+	  to: 'Galle'
+	}, {
+	  type: 'bus',
+	  from: 'Ella',
+	  to: 'Tissamahaharama'
+	}, {
+	  type: 'bus',
+	  from: 'Galle',
+	  to: 'Tissamahaharama'
+	}]);
 
 /***/ },
 /* 2 */
